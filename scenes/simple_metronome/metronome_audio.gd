0560d105258915eab270b45e2ece6f7d9e1ba0cc
c0linw <41310013+c0linw@users.noreply.github.com>
2024-08-14 00:30:25 -0700

    feat(metronome): list click sounds and automatically load them all in metronome_audio.gd:_ready()

--- scenes/simple_metronome/metronome_audio.gd
+++ scenes/simple_metronome/metronome_audio.gd
@@ -1,14 +1,21 @@
 extends Node
 
-var click_sources = {}
+var click_sources = {
+	"sin_800hz_4ms": null,
+	"sin_1600hz_4ms": null,
+	"tri_800hz_8ms": null,
+	"tri_1600hz_8ms": null,
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var click1 = FileAccess.open("res://sounds/tri_1600hz_8ms.wav",FileAccess.READ)
-	var buffer = click1.get_buffer(click1.get_length())
-	click1.close()
-	var click_source = Shinobu.register_sound_from_memory("tri_1600hz_8ms",buffer)
-	click_sources["tri_1600hz_8ms"] = click_source
+	for key in click_sources.keys():
+		var file = FileAccess.open("res://sounds/%s.wav" % key, FileAccess.READ)
+		print("res://sounds/%s.wav" % key)
+		var buffer = file.get_buffer(file.get_length())
+		file.close()
+		var click_source = Shinobu.register_sound_from_memory(key, buffer)
+		click_sources[key] = click_source
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,6 +23,6 @@ func _process(delta):
 
 
 func _on_metronome_beat(beat: int, next_beat_time_msec: int):
-	var sfx_player = click_sources["tri_1600hz_8ms"].instantiate(ShinobuGlobals.sfx_group)
+	var sfx_player = click_sources["sin_800hz_4ms"].instantiate(ShinobuGlobals.sfx_group)
 	sfx_player.schedule_start_time(next_beat_time_msec)
 	sfx_player.start()
